88cb16fbde37d6accf0692411d45949ef93642ba
Musketeer.Liu <Musketeer.Liu@gmail.com>
2019-12-23 00:06:08 -0800

    Shortcut With ListView

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
