8ed76d3d1ce49ef42419d45e30768cbff309de0b
Musketeer.Liu <Musketeer.Liu@gmail.com>
2019-12-23 00:12:05 -0800

    Testing Module

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 
 def vote(request, question_id):
@@ -26,6 +28,10 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
